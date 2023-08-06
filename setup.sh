1ca6ab7f5b5d6d557c2aaa3e1657daa655f8d90b
zecko8 <58081922+zecko8@users.noreply.github.com>
2023-08-06 09:53:45 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+chmod -R 777 /var/www
+echo Servername localhost >> /etc/apache2/apache2.conf
+echo 'Listen 81' >> /etc/apache2/ports.conf
+sed -i 's/80/81/' /etc/apache2/sites-enabled/000-default.conf
+
+docker-entrypoint.sh
