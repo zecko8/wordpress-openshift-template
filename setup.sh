13a5102e1c1e9002dc24d6552b97af449bfab478
zecko8 <58081922+zecko8@users.noreply.github.com>
2023-08-06 10:37:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 chmod -R 777 /var/www
 echo Servername localhost >> /etc/apache2/apache2.conf
-echo 'Listen 81' >> /etc/apache2/ports.conf
+sed -i 's/80/81/' /etc/apache2/ports.conf
 sed -i 's/80/81/' /etc/apache2/sites-enabled/000-default.conf
 
-docker-entrypoint.sh
+docker-entrypoint.sh apache2-foreground
